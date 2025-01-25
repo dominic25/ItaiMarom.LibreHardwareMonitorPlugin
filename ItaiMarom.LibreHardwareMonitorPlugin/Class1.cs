ae7dbf47b89efece200cb1375fcfee36bb7ddc29
dominic25 <itaimarom@gmail.com>
2025-01-25 20:41:18 +0200

    Add project files.

---
+++ ItaiMarom.LibreHardwareMonitorPlugin/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using SuchByte.MacroDeck.Plugins;
+using System.Reflection;
+using System.Threading.Tasks;
+using SuchByte.MacroDeck.Logging;
+namespace ItaiMarom.LibreHardwareMonitorPlugin
+{
+    public class LibreHardwareMonitorPlugin : MacroDeckPlugin
+    {
+        internal static MacroDeckPlugin Instance { get; set; }
+        private const int UpdateInterval = 1000;//should be configurable 
+
+        // Optional; If your plugin can be configured, set to "true". It'll make the "Configure" button appear in the package manager.
+        public override bool CanConfigure => false;
+
+        // Gets called when the plugin is loaded
+        public override void Enable()
+        {
+            Instance = this;
+            MacroDeckLogger.Trace(Instance, "started"); // Log a trace (loglevel 1)
+            Task.Run(async () => await DoWork());
+        }
+
+        //// Optional; Gets called when the user clicks on the "Configure" button in the package manager; If CanConfigure is not set to true, you don't need to add this
+        //public override void OpenConfigurator()
+        //{
+        //    // Open your configuration form here
+        //    using (var configurator = new Configurator())
+        //    {
+        //        configurator.ShowDialog();
+        //    }
+        //}
+
+        private static async Task DoWork()
+        {
+            while (true)
+            {
+                MacroDeckLogger.Trace(Instance, "DoWork"); // Log a trace (loglevel 1)
+                LoadDllAndMonitor(Instance);
+                await Task.Delay(TimeSpan.FromMilliseconds(UpdateInterval));
+            }
+        }
+
+        private static void LoadDllAndMonitor(MacroDeckPlugin Instance)
+        {
+            try
+            {
+                // Path to the DLL
+                string dllName = @"liberHardwareMonitorHelper.dll";
+                string dllPath = @"%Appdata%\Macro Deck\plugins\ItaiMarom.LibreHardwareMonitorPlugin\";
+
+                // Expand the environment variable
+                string expandedPath = Environment.ExpandEnvironmentVariables(dllPath + dllName);
+                // Load the DLL
+                Assembly assembly = Assembly.LoadFrom(expandedPath);
+
+                // Get the type of the class you want to use
+                Type myClassType = assembly.GetType("liberHardwareMonitorHelper.liberHardwareMonitorHelper");
+
+                if (myClassType != null)
+                {
+                    // Create an instance of the class
+                    object myClassInstance = Activator.CreateInstance(myClassType);
+
+                    // Get the method you want to invoke
+                    MethodInfo myMethod = myClassType.GetMethod("Monitor");
+
+                    if (myMethod != null)
+                    {
+                        // Call the method with parameters
+                        myMethod.Invoke(myClassInstance, new object[] { Instance });
+                    }
+                    else
+                    {
+                        MacroDeckLogger.Error(Instance, "Method not found.");
+
+                    }
+                }
+                else
+                {
+                    MacroDeckLogger.Error(Instance, "Class not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MacroDeckLogger.Error(Instance, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ liberHardwareMonitorHelper/Class1.cs
@@ -0,0 +1,67 @@
+﻿using LibreHardwareMonitor.Hardware;
+using SuchByte.MacroDeck.Logging;
+using SuchByte.MacroDeck.Plugins;
+using SuchByte.MacroDeck.Variables;
+
+namespace liberHardwareMonitorHelper
+{
+    public class liberHardwareMonitorHelper
+    {
+        public static void Monitor(MacroDeckPlugin Instance)
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                foreach (IHardware subhardware in hardware.SubHardware)
+                {
+                    foreach (ISensor sensor in subhardware.Sensors)
+                    {
+                        if (sensor.Value != null)
+                        {
+                            String name = sensor.Name + "_" + sensor.SensorType.ToString();
+                            VariableManager.SetValue(name, sensor.Value, VariableType.Float, Instance, null);
+                        }
+                    }
+                }
+
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    if (sensor.Value != null)
+                    {
+                        String name = sensor.Name + "_" + sensor.SensorType.ToString();
+                        VariableManager.SetValue(name, sensor.Value, VariableType.Float, Instance, null);
+                    }
+                }
+            }
+            computer.Close();
+        }
+    }
+
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+}
